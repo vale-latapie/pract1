79cdc1cd6aee35e92a81acd8bef64e1ce467fea5
Juanrobledo100 <robledojuan100@gmail.com>
2023-12-04 17:51:20 -0300

    ejercicio2 realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-function App() {
+import 'bootstrap'
+
+function App(props) {
   
 
   return (
     <>
-     
+     <h1 className='text-center'>{props}</h1>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App props={"Hello My Friend"} />
   </React.StrictMode>,
 )
